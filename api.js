aa170b3f6c33b82947ffac0d211b518fd441b463
simo255 <bmohamed255@gmail.com>
2024-09-09 01:44:44 -0400

    better prompt

--- api.js
+++ api.js
@@ -59,18 +59,14 @@ function getSystemContent(jobDescription) {
     const stmt0 = "You are  a Talent Acquisition Specialist, you have to select the best candidate for a job application, you have received a resume from a candidate, but it's not tailored to the job description, you have to generate a new resume for the candidate.";
     const stmt1 = "This is the job description : " + jobDescription + ", You have to generate a resume for this job application.";
     const stmt3 = "You will generate a new resume for me using the appropriate keywords for a better match.";
+    const stmt16 = "According to my resume infos, you have to find a way to make it more relevant to the job description. As if I truly have the experience and the skills required for the job.";
+    const stmt5 = "if some informations are not really relevant, you can skip it, or at least add some bullet points that may fit to the job description.";
     const stmt4 = "Add the keywords inside the experience, education and projects sections of my resume. Make them in bold format.";
-    const stmt5 = "If the keywords were not present in my resume, add them in the appropriate experience detail.";
     const stmt6 = "The resume must be in LaTeX format.";
-    const stmt8 = "If the experience and education in my resume is not relevant to the job description, keep the work experience but change the details, these details must be related to the job description and have the keywords.";
     const stmt9 = "Make sure it's a one page format, the length of the answer can be up to 6500 characters, without spaces, but not less than 5500, this is mandatory.";
-    const stmt10 = "if a keyword is present in my resume and the job description, You have to give more details about it in the resume that you will generate.";
     const stmt11 = "If my resume has a skills section, don't add the keywords if it's already present in other sections. YOU have to add only the missing keywords in the skills section.";
-    const stmt12 = "### for example, if the job description is asking for a software engineer with experience in Angular, You have to talk about Angular, TypeScript, JavaScript, html, css, and other related technologies in the resume that you will generate.";
-    const stmt14 = "Your answer must be in the following format : ```latex Your answer ```";
-    const stmt13 = " If the job description is asking for a software engineer with experience in Python, You have to talk about FastApi, Pycharm, Data visualization, and other related technologies in the resume that you will generate. ###";
-    const stmt15 = "in the skill section, do not add the keywords if it's already mentioned.";
-    return stmt0 + stmt1 + stmt10 + stmt3 + stmt4 + stmt5 + stmt6 + stmt9 + stmt8 + stmt11 + stmt12 + stmt13 + stmt14 + stmt15;
+    const stmt14 = "Your answer must be in the following format : ```latex Your answer ```.";
+    return stmt0 + stmt1  + stmt3 + stmt4  + stmt6 + stmt9  + stmt11  + stmt14 + stmt16 + stmt5;
 }
 
 
